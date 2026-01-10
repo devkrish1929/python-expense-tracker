b731a01b3de9b81e6c0ee9e33e0447b58d63a19e
devkrish1926 <krishna.71906@gmail.com>
2026-01-10 22:37:03 +0530

    Add Functionality to save expenses to file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Expense Tracker Started")
+
+amount = input("Enter expense amount: ")
+category = input("Enter expense category: ")
+
+with open("expenses.txt", "a") as file:
+    file.write(amount + "," + category + "\n")
+
+print("Expense saved successfully!")
