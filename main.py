21c0bd5334fd35b083fd26619faa315829c5688f
devkrish1926 <krishna.71906@gmail.com>
2026-01-10 23:02:56 +0530

    Add expense input ,storage and display

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Expense Tracker Started")
+print("Expense Tracker")
 
 amount = input("Enter expense amount: ")
 category = input("Enter expense category: ")
@@ -7,3 +7,16 @@ with open("expenses.txt", "a") as file:
     file.write(amount + "," + category + "\n")
 
 print("Expense saved successfully!")
+
+print("\nAll Expenses:")
+
+total = 0
+
+with open("expenses.txt", "r") as file:
+    for line in file:
+        amount, category = line.strip().split(",")
+        total += float(amount)
+        print(f"Amount: {amount} | Category: {category}")
+
+print("------------------------")
+print("Total spent:", total)
